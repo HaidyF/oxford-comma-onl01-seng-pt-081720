8127baa59116a1dc54fc433d74437c86fb28a9a4
Haidy  Farahat <haidyfarahat@gmail.com>
2020-08-26 21:56:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,6 @@ def oxford_comma(array)
     print array [0]
   elsif array.length == 2
     print array.join(" and ")
-  else array.length > 2
+  elsif array.length > 2
     print array.slice (0, l-1).join(" , ") + " and " + array [-1]
 end
