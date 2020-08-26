394a4cf09dfd68a226d5e637b542fd23f273f875
Haidy  Farahat <haidyfarahat@gmail.com>
2020-08-26 22:04:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length > 2
     x = array.slice (0..array.length-2)
-    y = x.join(" , ")
+    y = x.join(", ")
     z = array [-1]
     return y + ", and " + z
   else
